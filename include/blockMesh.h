3b9e659c613acea2e0242690edc6fa88f14780f4
sohail69 <73966892+sohail69@users.noreply.github.com>
2024-09-17 11:19:56 +0100

    Update blockMesh.h

--- include/blockMesh.h
+++ include/blockMesh.h
@@ -2,14 +2,17 @@
 #include<cmath>
 
 //
-// produces a 3-D bliockmesh
+// produces a 3-D blockmesh
+// does not store coords etc..
+// keeps a record of local partition
+// and calculates on the fly
 //
 template<typename num, typename iter>
 class blockMesh{
   private:
     iter nx, ny, nz;    //Number of nodes in each direction
     iter nxe, nye, nze; //Number of elements in each direction
-
+    num dx, dy, dz;     //the uniform sizes in each direction
   public:
 
 }
