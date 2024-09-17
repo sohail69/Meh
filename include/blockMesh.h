7e6eb617e2b1e41db97e64efebf81a057657b94d
sohail69 <73966892+sohail69@users.noreply.github.com>
2024-09-17 11:18:11 +0100

    Update blockMesh.h

--- include/blockMesh.h
+++ include/blockMesh.h
@@ -1,10 +1,14 @@
 #include <vector>
 #include<cmath>
 
-
+//
+// produces a 3-D bliockmesh
+//
 template<typename num, typename iter>
 class blockMesh{
   private:
+    iter nx, ny, nz;    //Number of nodes in each direction
+    iter nxe, nye, nze; //Number of elements in each direction
 
   public:
 
