c5b14ce6034014bf83cad5fa45e9554b02c9ff41
sohail69 <73966892+sohail69@users.noreply.github.com>
2024-09-17 12:00:10 +0100

    Update blockMesh.h

--- include/blockMesh.h
+++ include/blockMesh.h
@@ -7,15 +7,16 @@
 // keeps a record of local partition
 // and calculates on the fly
 //
-template<typename num, typename iter>
+template<typename num, typename iter, class partitioner>
 class blockMesh{
   private:
     num dx[3];          //The resolution of the blockmesh
     iter nx[3], nxe[3]; //The size of the blockmesh
     iter nStart, nEnd;  //Local start and end nodes
     iter neStart, neEnd;//Local start and end element
+    partitioner Parti;
   public:
-    blockMesh(){
+    blockMesh(iter n[3], num dx[3], part){
 
     }
 
